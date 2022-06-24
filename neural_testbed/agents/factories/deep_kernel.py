40e71a7ecf42a8b5df3524a2355d08ab6299bdff
Mohammad Asghari <smasghari@google.com>
2022-06-24 07:17:55 -0700

    Internal change
    PiperOrigin-RevId: 457005819
    Change-Id: Ie52f500cc53d6161f57a7bf04402357a7dcc7b80

--- neural_testbed/agents/factories/deep_kernel.py
+++ neural_testbed/agents/factories/deep_kernel.py
@@ -173,12 +173,15 @@ def make_agent(config: DeepKernelConfig) -> testbed_base.TestbedAgent:
         config.weight_decay * jnp.sqrt(prior.temperature) * prior.input_dim /
         prior.num_train)
 
-    single_loss = losses.combine_single_index_losses_no_state_as_metric(
+    single_loss = losses.combine_single_index_losses_as_metric(
         # This is the loss you are training on.
-        train_loss=losses.XentLoss(prior.num_classes),
+        train_loss=losses.XentLossWithState(prior.num_classes),
         # We will also log the accuracy in classification.
-        extra_losses={'acc': losses.AccuracyErrorLoss(prior.num_classes)},
+        extra_losses={
+            'acc': losses.AccuracyErrorLossWithState(prior.num_classes)
+        },
     )
+    single_loss = losses.wrap_single_loss_as_single_loss_no_state(single_loss)
     loss_fn = losses.average_single_index_loss_no_state(single_loss,)
     loss_fn = losses.add_l2_weight_decay_no_state(loss_fn, scale=weight_decay)
     loss_fn = jax.jit(functools.partial(loss_fn, net))
